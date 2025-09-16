eb896d53745c9122184857c3d5d3c9dd6cf98649
DabeenKim <81741760+beencoder@users.noreply.github.com>
2025-09-16 19:22:20 +0900

    feat: meal 상세페이지 작업

--- next-community-app/lib/meals.js
+++ next-community-app/lib/meals.js
@@ -1,8 +1,13 @@
 import Database from 'better-sqlite3';
+
 const db = new Database('meals.db');
 
 export async function getMeals() {
   await new Promise((resolve) => setTimeout(resolve, 2000));
   //throw new Error('Loading meals failed');
   return db.prepare('SELECT * FROM meals').all();
+}
+
+export function getMeal(slug) {
+  return db.prepare('SELECT * FROM meals WHERE slug = ?').get(slug);
 }
