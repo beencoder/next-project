677ebbe2999846bd5b0936b654c852610cf0bea9
DabeenKim <81741760+beencoder@users.noreply.github.com>
2025-09-21 19:55:07 +0900

    feat: 입력폼 중 빈값일 경우 validation(유효성 검사) 추가

--- next-community-app/app/meals/share/page.js
+++ next-community-app/app/meals/share/page.js
@@ -1,9 +1,14 @@
+'use client';
+
+import { useActionState } from 'react';
+
 import classes from './page.module.css';
 import { shareMeal } from '@/lib/actions';
 import ImagePicker from '@/components/meals/image-picker';
 import MealsFormSubmit from '@/components/meals/meals-form-submit';
 
 export default function ShareMealPage() {
+  const [state, formAction] = useActionState(shareMeal, { message: null });
   return (
     <>
       <header className={classes.header}>
@@ -13,30 +18,36 @@ export default function ShareMealPage() {
         <p>Or any other meal you feel needs sharing!</p>
       </header>
       <main className={classes.main}>
-        <form className={classes.form} action={shareMeal}>
+        <form className={classes.form} action={formAction}>
           <div className={classes.row}>
             <p>
               <label htmlFor="name">Your name</label>
-              <input type="text" id="name" name="name" required />
+              <input type="text" id="name" name="name" defaultValue={state.values?.creator || ''} required />
             </p>
             <p>
               <label htmlFor="email">Your email</label>
-              <input type="email" id="email" name="email" required />
+              <input type="email" id="email" name="email" defaultValue={state.values?.creator_email || ''} required />
             </p>
           </div>
           <p>
             <label htmlFor="title">Title</label>
-            <input type="text" id="title" name="title" required />
+            <input type="text" id="title" name="title" defaultValue={state.values?.title || ''} required />
           </p>
           <p>
             <label htmlFor="summary">Short Summary</label>
-            <input type="text" id="summary" name="summary" required />
+            <input type="text" id="summary" name="summary" defaultValue={state.values?.summary || ''} required />
           </p>
           <p>
             <label htmlFor="instructions">Instructions</label>
-            <textarea id="instructions" name="instructions" rows="10" required></textarea>
+            <textarea
+              id="instructions"
+              name="instructions"
+              rows="10"
+              defaultValue={state.values?.instructions || ''}
+              required></textarea>
           </p>
           <ImagePicker label="Your image" name="image" />
+          {state.message && <p className={classes.validation}>{state.message}</p>}
           <p className={classes.actions}>
             <MealsFormSubmit />
           </p>
