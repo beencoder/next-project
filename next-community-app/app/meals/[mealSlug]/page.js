a4936655ad875a9812bcbfc64ab1ea7dac9d4c14
DabeenKim <81741760+beencoder@users.noreply.github.com>
2025-09-22 16:22:27 +0900

    feat: meals 관련 페이지 메타데이터 추가

--- next-community-app/app/meals/[mealSlug]/page.js
+++ next-community-app/app/meals/[mealSlug]/page.js
@@ -1,8 +1,22 @@
 import Image from 'next/image';
 import { notFound } from 'next/navigation';
 
-import classes from './page.module.css';
 import { getMeal } from '@/lib/meals';
+import classes from './page.module.css';
+
+export async function generateMetadata({ params }) {
+  const { mealSlug } = await params;
+  const meal = getMeal(mealSlug);
+
+  if (!meal) {
+    notFound();
+  }
+
+  return {
+    title: meal.title,
+    description: meal.summary,
+  };
+}
 
 export default async function MealDetailsPage({ params }) {
   const { mealSlug } = await params;
