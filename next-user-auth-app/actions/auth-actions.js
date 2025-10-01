3c4d117bfc6fc01a78b151d8851c9fcf3474d651
DabeenKim <81741760+beencoder@users.noreply.github.com>
2025-10-01 19:19:30 +0900

    feat: db에 사용자 저장 동작 추가 및 이메일, 비밀번호 유효성 검사 추가

---
+++ next-user-auth-app/actions/auth-actions.js
@@ -0,0 +1,43 @@
+'use server';
+
+import { redirect } from 'next/navigation';
+
+import { hashUserPassword } from '@/lib/hash';
+import { createUser } from '@/lib/user';
+import { createAuthSession } from '@/lib/auth';
+
+export async function signup(prevState, formData) {
+  const email = formData.get('email');
+  const password = formData.get('password');
+  let errors = {};
+
+  if (!email.includes('@')) {
+    errors.email = '유효한 이메일 주소를 입력해 주세요.';
+  }
+  if (password.trim().length < 8) {
+    errors.password = '비밀번호는 최소 8자 이상이어야 합니다.';
+  }
+  if (Object.keys(errors).length > 0) {
+    return {
+      errors,
+    };
+  }
+
+  // 사용자 추가
+  const hashedPassword = hashUserPassword(password);
+  try {
+    const id = createUser(email, hashedPassword);
+    await createAuthSession(id);
+    redirect('/training');
+    s;
+  } catch (error) {
+    if (error.code === 'SQLITE_CONSTRAINT_UNIQUE') {
+      return {
+        errors: {
+          email: '입력하신 이메일로 이미 계정이 존재하는 것 같습니다.',
+        },
+      };
+    }
+    throw error;
+  }
+}
---
+++ next-user-auth-app/lib/user.js
@@ -0,0 +1,7 @@
+import db from './db';
+
+export function createUser(email, password) {
+  const result = db.prepare('INSERT INTO users (email, password) VALUES (?, ?)').run(email, password);
+
+  return result.lastInsertRowid; // 마지막으로 추가된 행의 고유 ID
+}
